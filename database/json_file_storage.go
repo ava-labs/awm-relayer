39fdc21ffbe6f94bd8e96d131c785cf7ed28d069
cam-schultz <camschultz32@gmail.com>
2023-08-30 13:46:30 +0000

    better read error handling

--- database/json_file_storage.go
+++ database/json_file_storage.go
@@ -14,6 +14,10 @@ import (
 
 var _ RelayerDatabase = &JsonFileStorage{}
 
+var (
+	fileDoesNotExistErr = errors.New("JSON database file does not exist")
+)
+
 type chainState map[string]string
 
 // JsonFileStorage implements RelayerDatabase
@@ -21,16 +25,14 @@ type JsonFileStorage struct {
 	// the directory where the json files are stored
 	dir string
 
-	// one mutex per network
-	// we predefined the network when creating the storage
-	// also we don't allow adding new network after the storage is created
-	// so we don't need to lock the map
+	// Each network has its own mutex
+	// The chainIDs used to index the JsonFileStorage are created at initialization
+	// and are not modified afterwards, so we don't need to lock the map itself.
 	mutexes map[ids.ID]*sync.RWMutex
 	logger  logging.Logger
 }
 
 // NewJSONFileStorage creates a new JsonFileStorage instance
-// with predefined networks. e.g "ethereum", "avalanche"
 func NewJSONFileStorage(logger logging.Logger, dir string, networks []ids.ID) (*JsonFileStorage, error) {
 	storage := &JsonFileStorage{
 		dir:     filepath.Clean(dir),
@@ -64,9 +66,12 @@ func NewJSONFileStorage(logger logging.Logger, dir string, networks []ids.ID) (*
 // Get the latest chain state from the json database, and retrieve the value from the key
 func (s *JsonFileStorage) Get(chainID ids.ID, key []byte) ([]byte, error) {
 	var currentState chainState
-	err := s.read(chainID, &currentState)
+	fileExists, err := s.read(chainID, &currentState)
 	if err != nil {
 		return nil, err
+	}
+	if !fileExists {
+		return nil, fileDoesNotExistErr
 	}
 
 	return []byte(currentState[string(key)]), nil
@@ -76,8 +81,17 @@ func (s *JsonFileStorage) Get(chainID ids.ID, key []byte) ([]byte, error) {
 func (s *JsonFileStorage) Put(chainID ids.ID, key []byte, value []byte) error {
 	currentState := make(chainState)
 	var currentStateData []byte
-	err := s.read(chainID, &currentStateData)
-	if err == nil {
+	fileExists, err := s.read(chainID, &currentStateData)
+	if err != nil {
+		s.logger.Error(
+			"failed to read file",
+			zap.Error(err),
+		)
+		return err
+	}
+	// If the file exists, unmarshal the data into currentState
+	// Otherwise, we write the value to the empty currentState, and overwrite the file
+	if fileExists {
 		err = json.Unmarshal(currentStateData, &currentState)
 		if err != nil {
 			return err
@@ -124,30 +138,40 @@ func (s *JsonFileStorage) write(network ids.ID, v interface{}) error {
 }
 
 // Read from disk and unmarshal into v
-func (s *JsonFileStorage) read(network ids.ID, v interface{}) error {
+// Returns a bool indicating whether the file exists, and an error.
+// If an error is returned, the bool should be ignored.
+func (s *JsonFileStorage) read(network ids.ID, v interface{}) (bool, error) {
 	mutex, ok := s.mutexes[network]
 	if !ok {
-		return errors.New("network does not exist")
+		return false, errors.New("network does not exist")
 	}
 
 	mutex.RLock()
 	defer mutex.RUnlock()
 
 	path := filepath.Join(s.dir, network.String()+".json")
-	_, err := os.Stat(path)
-	if err != nil {
-		return err
+
+	// If the file does not exist, return false, but do not return an error as this
+	// is an expected case
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		s.logger.Debug(
+			"file does not exist",
+			zap.String("path", path),
+			zap.Error(err),
+		)
+		return false, nil
 	}
 
 	b, err := os.ReadFile(path)
 	if err != nil {
-		return errors.Wrap(err, "failed to read file")
+		return false, errors.Wrap(err, "failed to read file")
 	}
 
 	// unmarshal data
 	if err = json.Unmarshal(b, &v); err != nil {
-		return errors.Wrap(err, "failed to unmarshal json file")
+		return false, errors.Wrap(err, "failed to unmarshal json file")
 	}
 
-	return nil
+	// Return true to indicate that the file exists and we read from it successfully
+	return true, nil
 }
