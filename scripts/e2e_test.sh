3c6985b7ebc7268e06de6715ef4be7ede1feb6db
Matthew Lam <matthew.lam@avalabs.org>
2023-08-31 18:20:43 +0000

    adding e2e test to ci

--- scripts/e2e_test.sh
+++ scripts/e2e_test.sh
@@ -10,11 +10,6 @@ source "$AWM_RELAYER_PATH"/scripts/constants.sh
 
 source "$AWM_RELAYER_PATH"/scripts/versions.sh
 
-BASEDIR=/tmp/e2e-test AVALANCHEGO_BUILD_PATH=/tmp/e2e-test/avalanchego ./scripts/install_avalanchego_release.sh
-./scripts/build.sh /tmp/e2e-test/avalanchego/plugins/srEXiWaHuhNyGwPUi444Tu47ZEDwxTWrbQiuD7FmgSAQ6X7Dy
-AVALANCHEGO_BUILD_PATH=/tmp/e2e-test/avalanchego DATA_DIR=/tmp/e2e-test/data ./scripts/run_ginkgo.sh
-DATA_DIR=/tmp/e2e-test/data
-
 # Build ginkgo
 # to install the ginkgo binary (required for test build and run)
 go install -v github.com/onsi/ginkgo/v2/ginkgo@${GINKGO_VERSION}
