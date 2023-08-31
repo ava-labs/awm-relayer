9a5172786ebcff3f5364059f5f4bc558bc80973f
cam-schultz <camschultz32@gmail.com>
2023-08-31 14:29:14 +0000

    more unit tests

--- database/json_file_storage_test.go
+++ database/json_file_storage_test.go
@@ -13,7 +13,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConcurrentWriteRead(t *testing.T) {
+// Test that the JSON database can write and read to a single chain concurrently.
+func TestConcurrentWriteReadSingleChain(t *testing.T) {
+	networks := []ids.ID{
+		ids.GenerateTestID(),
+	}
+	jsonStorage := setupJsonStorage(t, networks)
+
+	// Test writing to the JSON database concurrently.
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			testWrite(jsonStorage, networks[0], uint64(i))
+		}()
+	}
+	wg.Wait()
+
+	// Write one final time to ensure that concurrent writes don't cause any issues.
+	finalTargetValue := uint64(11)
+	testWrite(jsonStorage, networks[0], finalTargetValue)
+
+	latestSeenBlockData, err := jsonStorage.Get(networks[0], []byte(LatestSeenBlockKey))
+	if err != nil {
+		t.Fatal(fmt.Sprintf("failed to retrieve from JSON storage. err: %v", err))
+	}
+	latestSeenBlock, success := new(big.Int).SetString(string(latestSeenBlockData), 10)
+	if !success {
+		t.Fatal(fmt.Sprintf("failed to convert latest block to big.Int. err: %v", err))
+	}
+	assert.Equal(t, finalTargetValue, latestSeenBlock.Uint64(), "latest seen block height is not correct.")
+
+}
+
+// Test that the JSON database can write and read from multiple chains concurrently. Write to any given chain are not concurrent.
+func TestConcurrentWriteReadMultipleChains(t *testing.T) {
 	networks := []ids.ID{
 		ids.GenerateTestID(),
 		ids.GenerateTestID(),
@@ -25,14 +60,19 @@ func TestConcurrentWriteRead(t *testing.T) {
 	wg := sync.WaitGroup{}
 	for i := 0; i < 3; i++ {
 		wg.Add(1)
+		index := i
 		go func() {
 			defer wg.Done()
-			testWrite(jsonStorage, networks[0], uint64(0))
-			testWrite(jsonStorage, networks[1], uint64(1))
-			testWrite(jsonStorage, networks[2], uint64(2))
+			testWrite(jsonStorage, networks[index], uint64(index))
 		}()
 	}
 	wg.Wait()
+
+	// Write one final time to ensure that concurrent writes don't cause any issues.
+	finalTargetValue := uint64(3)
+	for _, network := range networks {
+		testWrite(jsonStorage, network, finalTargetValue)
+	}
 
 	for i, id := range networks {
 		latestSeenBlockData, err := jsonStorage.Get(id, []byte(LatestSeenBlockKey))
@@ -43,7 +83,7 @@ func TestConcurrentWriteRead(t *testing.T) {
 		if !success {
 			t.Fatal(fmt.Sprintf("failed to convert latest block to big.Int. err: %v", err))
 		}
-		assert.Equal(t, uint64(i), latestSeenBlock.Uint64(), fmt.Sprintf("latest seen block height is not correct. networkID: %d", i))
+		assert.Equal(t, finalTargetValue, latestSeenBlock.Uint64(), fmt.Sprintf("latest seen block height is not correct. networkID: %d", i))
 	}
 }
 
