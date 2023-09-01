4951363bec9a4a92261aa240daf3f4e03746ef0d
Cameron Schultz <camschultz32@gmail.com>
2023-09-01 14:10:41 -0400

    source constants.sh in unit tests

---
+++ scripts/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+# See the file LICENSE for licensing terms.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Directory above this script
+RELAYER_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
+
+source "$RELAYER_PATH"/scripts/constants.sh
+
+go test ./...
