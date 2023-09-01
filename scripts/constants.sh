189141901a60217a080f8a3af0a6231bae9ca714
Cameron Schultz <camschultz32@gmail.com>
2023-09-01 14:10:14 -0400

    export portable blst vars

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -22,3 +22,12 @@ relayer_dockerhub_repo=${DOCKER_REPO:-"awm-relayer"}
 current_branch=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match || true)
 
 git_commit=${RELAYER_COMMIT:-$( git rev-list -1 HEAD )}
+
+# Set the CGO flags to use the portable version of BLST
+#
+# We use "export" here instead of just setting a bash variable because we need
+# to pass this flag to all child processes spawned by the shell.
+export CGO_CFLAGS="-O -D__BLST_PORTABLE__"
+# While CGO_ENABLED doesn't need to be explicitly set, it produces a much more
+# clear error due to the default value change in go1.20.
+export CGO_ENABLED=1
