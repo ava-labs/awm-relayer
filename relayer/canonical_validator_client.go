e398734f8cf6377c337d299129a7d5879a885082
Michael Kaplan <michael@avalabs.org>
2023-10-31 15:16:32 -0400

    Add comment explaining GetValidatorSet implementation

--- relayer/canonical_validator_client.go
+++ relayer/canonical_validator_client.go
@@ -36,10 +36,11 @@ func (v *CanonicalValidatorClient) GetSubnetID(ctx context.Context, chainID ids.
 
 // Gets the current validator set of the given subnet ID, include the validators' BLS public keys.
 // This implementation of GetValidatorSet currently makes two RPC requests, one to get the
-// subnet validators, and another to get their BLS public keys. This is necessary because BLS keys
-// are currently only associated with primary network validation periods. If ACP-13 is implementated
-// in the future (https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/13-subnet-only-validators.md),
-// it may become possible to reduce this to a single RPC request that returns both the subnet validators
+// subnet validators, and another to get their BLS public keys. This is necessary in order to enable
+// the use of the public APIs (which don't support "GetValidatorsAt") because BLS keys are currently
+// only associated with primary network validation periods. If ACP-13 is implementated in the future
+// (https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/13-subnet-only-validators.md), it may
+// become possible to reduce this to a single RPC request that returns both the subnet validators
 // as well as their BLS public keys.
 func (v *CanonicalValidatorClient) GetValidatorSet(
 	ctx context.Context,
