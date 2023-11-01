7bd663c23f6293891e5a46eea25c7f8aeed3a0e9
Michael Kaplan <michael@avalabs.org>
2023-11-01 11:32:22 -0400

    Use getCurrentValidators as a fallback. Proper nil checks. Update comments

--- relayer/canonical_validator_client.go
+++ relayer/canonical_validator_client.go
@@ -45,11 +45,9 @@ func (v *CanonicalValidatorClient) GetSubnetID(ctx context.Context, chainID ids.
 // (https://github.com/avalanche-foundation/ACPs/blob/main/ACPs/13-subnet-only-validators.md), it may
 // become possible to reduce this to a single RPC request that returns both the subnet validators
 // as well as their BLS public keys.
-func (v *CanonicalValidatorClient) GetValidatorSet(
+func (v *CanonicalValidatorClient) getCurrentValidatorSet(
 	ctx context.Context,
-	height uint64,
-	subnetID ids.ID,
-) (map[ids.NodeID]*validators.GetValidatorOutput, error) {
+	subnetID ids.ID) (map[ids.NodeID]*validators.GetValidatorOutput, error) {
 	// Get the current subnet validators. These validators are not expected to include
 	// BLS signing information given that addPermissionlessValidatorTx is only used to
 	// add primary network validators.
@@ -76,16 +74,45 @@ func (v *CanonicalValidatorClient) GetValidatorSet(
 
 	// Set the BLS keys of the result.
 	for _, primaryVdr := range primaryVdrs {
+		// We expect all of the primary network validators to already be in `res` because
+		// we filtered the request to node IDs that were identified as validators of the
+		// specific subnet ID.
 		vdr, ok := res[primaryVdr.NodeID]
 		if !ok {
 			v.logger.Warn(
-				"Missing primary network validator for subnet validator",
+				"Unexpected primary network validator returned by getCurrentValidators request",
 				zap.String("subnetID", subnetID.String()),
 				zap.String("nodeID", primaryVdr.NodeID.String()))
 			continue
 		}
-		vdr.PublicKey = primaryVdr.Signer.Key()
+
+		// Validators that do not have a BLS public key registered on the P-chain are still
+		// included in the result because they affect the stake weight of the subnet validators.
+		// Such validators will not be queried for BLS signatures of warp messages. As long as
+		// sufficient stake percentage of subnet validators have registered BLS public keys,
+		// messages can still be successfully relayed.
+		if primaryVdr.Signer != nil {
+			vdr.PublicKey = primaryVdr.Signer.Key()
+		}
 	}
 
+	return res, nil
+}
+
+func (v *CanonicalValidatorClient) GetValidatorSet(
+	ctx context.Context,
+	height uint64,
+	subnetID ids.ID,
+) (map[ids.NodeID]*validators.GetValidatorOutput, error) {
+	// First, attempt to use the "getValidatorsAt" RPC method. This method may not be available on
+	// all API nodes, in which case we can fall back to using "getCurrentValidators" if needed.
+	res, err := v.client.GetValidatorsAt(ctx, subnetID, height)
+	if err != nil {
+		v.logger.Debug(
+			"P-chain RPC to getValidatorAt returned error. Falling back to getCurrentValidators",
+			zap.String("subnetID", subnetID.String()),
+			zap.Uint64("pChainHeight", height))
+		return v.getCurrentValidatorSet(ctx, subnetID)
+	}
 	return res, nil
 }
