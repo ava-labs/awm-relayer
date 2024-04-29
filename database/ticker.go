fede82a9432b448658f6abc666ebbf74ebbf534e
cam-schultz <camschultz32@gmail.com>
2024-04-29 14:01:07 +0000

    add mutex

--- database/ticker.go
+++ database/ticker.go
@@ -3,20 +3,28 @@
 
 package database
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Ticker struct {
 	interval      time.Duration
 	subscriptions []chan struct{}
+	lock          *sync.Mutex
 }
 
 func NewTicker(writeIntervalSeconds uint64) *Ticker {
 	return &Ticker{
 		interval: time.Duration(writeIntervalSeconds) * time.Second,
+		lock:     &sync.Mutex{},
 	}
 }
 
 func (t *Ticker) Subscribe() chan struct{} {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
 	sub := make(chan struct{})
 	t.subscriptions = append(t.subscriptions, sub)
 	return sub
@@ -24,8 +32,10 @@ func (t *Ticker) Subscribe() chan struct{} {
 
 func (t *Ticker) Run() {
 	for range time.Tick(t.interval) {
+		t.lock.Lock()
 		for _, sub := range t.subscriptions {
 			sub <- struct{}{}
 		}
+		t.lock.Unlock()
 	}
 }
