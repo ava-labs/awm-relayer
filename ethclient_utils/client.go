7663a6fbad85c74eea42b85012254d011c5147b5
Nathan Haim <haim.nathan@icloud.com>
2024-05-05 22:14:06 +0200

    ethclient: add local ethclient This Makes the eth client configurable with query-params and http-headers

---
+++ ethclient_utils/client.go
@@ -0,0 +1,51 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package ethclient_utils
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/url"
+
+	"github.com/ava-labs/subnet-evm/ethclient"
+	"github.com/ava-labs/subnet-evm/rpc"
+)
+
+var ErrInvalidEndpoint = errors.New("invalid rpc endpoint")
+
+func DialWithConfig(ctx context.Context, endpoint string, httpHeaders, queryParams map[string]string) (ethclient.Client, error) {
+	endpoint, err := addQueryParams(endpoint, queryParams)
+	if err != nil {
+		return nil, err
+	}
+	client, err := rpc.DialOptions(ctx, endpoint, newClientOptions(httpHeaders)...)
+	if err != nil {
+		return nil, err
+	}
+	return ethclient.NewClient(client), nil
+}
+
+// addQueryParams adds the query parameters to the url
+func addQueryParams(endpoint string, queryParams map[string]string) (string, error) {
+	uri, err := url.ParseRequestURI(endpoint)
+	if err != nil {
+		return "", fmt.Errorf("%w: %v", ErrInvalidEndpoint, err)
+	}
+	values := uri.Query()
+	for key, value := range queryParams {
+		values.Add(key, value)
+	}
+	uri.RawQuery = values.Encode()
+	return uri.String(), nil
+}
+
+// newClientOptions creates a ClientOption slice from httpHeaders
+func newClientOptions(httpHeaders map[string]string) []rpc.ClientOption {
+	opts := make([]rpc.ClientOption, 0, len(httpHeaders))
+	for key, value := range httpHeaders {
+		opts = append(opts, rpc.WithHeader(key, value))
+	}
+	return opts
+}
---
+++ ethclient_utils/client_test.go
@@ -0,0 +1,45 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package ethclient_utils
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddQueryParams(t *testing.T) {
+	t.Run("NoQueryParams", func(t *testing.T) {
+		newurl, err := addQueryParams("https://avalabs.com", nil)
+		require.NoError(t, err)
+		require.Equal(t, "https://avalabs.com", newurl)
+	})
+	t.Run("TwoQueryParams", func(t *testing.T) {
+		newurl, err := addQueryParams("https://avalabs.com", map[string]string{
+			"first":  "value1",
+			"second": "value2",
+		})
+		require.NoError(t, err)
+		require.Equal(t, "https://avalabs.com?first=value1&second=value2", newurl)
+	})
+	t.Run("InvalidEndpoint", func(t *testing.T) {
+		_, err := addQueryParams("invalid-endpoint", nil)
+		require.True(t, errors.Is(err, ErrInvalidEndpoint))
+	})
+}
+
+func TestNewClientOptions(t *testing.T) {
+	t.Run("NoHttpHeaders", func(t *testing.T) {
+		opts := newClientOptions(nil)
+		require.Len(t, opts, 0)
+	})
+	t.Run("TwoHttpHeaders", func(t *testing.T) {
+		opts := newClientOptions(map[string]string{
+			"first":  "value1",
+			"second": "value2",
+		})
+		require.Len(t, opts, 2)
+	})
+}
