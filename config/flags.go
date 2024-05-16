7e8aad8b7bc502baed21437a012c9eb395636043
nathan haim <nathan.haim@free.fr>
2024-05-16 19:25:30 +0200

    Apply suggestions from code review
    Co-authored-by: cam-schultz <78878559+cam-schultz@users.noreply.github.com>
    Signed-off-by: nathan haim <nathan.haim@free.fr>

--- config/flags.go
+++ config/flags.go
@@ -8,6 +8,6 @@ import "github.com/spf13/pflag"
 func BuildFlagSet() *pflag.FlagSet {
 	fs := pflag.NewFlagSet("awm-relayer", pflag.ContinueOnError)
 	fs.String(ConfigFileKey, "", "Specifies the relayer config file")
-	fs.BoolP(VersionKey, "v", false, "Display awm-relayer version")
+	fs.BoolP(VersionKey, "", false, "Display awm-relayer version")
 	return fs
 }
