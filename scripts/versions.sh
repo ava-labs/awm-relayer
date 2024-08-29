b87bcc1275f90ea1f720e46c434cad5ba9ec4ca7
Ian Suvak <ian.suvak@avalabs.org>
2024-08-29 10:02:52 -0400

    Revert "use extract_commit instead of manually synced references in versions.sh"
    This reverts commit 47cfef6a6b259bffb04a75b97e108f2186bb8e25.

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -13,22 +13,18 @@ function getDepVersion() {
     grep -m1 "^\s*$1" $BASE_PATH/go.mod | cut -d ' ' -f2
 }
 
-function extract_commit() {
-  local version=$1
-  if [[ $version == *-* ]]; then
-      # Extract the substring after the last '-'
-      version=${version##*-}
-  fi
-  echo "$version"
-}
-
 # This needs to be exported to be picked up by the dockerfile.
 export GO_VERSION=${GO_VERSION:-$(getDepVersion go)}
 
 # Don't export them as they're used in the context of other calls
-AVALANCHEGO_VERSION=${AVALANCHEGO_VERSION:-$(extract_commit "$(getDepVersion github.com/ava-labs/avalanchego)")}
-GINKGO_VERSION=${GINKGO_VERSION:-$(extract_commit "$(getDepVersion github.com/onsi/ginkgo/v2)")}
-SUBNET_EVM_VERSION=${SUBNET_EVM_VERSION:-$(extract_commit "$(getDepVersion github.com/ava-labs/subnet-evm)")}
+# TODO: undo this hack once go.mod is referring to a tag rather than a commit
+#AVALANCHEGO_VERSION=${AVALANCHEGO_VERSION:-$(getDepVersion github.com/ava-labs/avalanchego)}
+AVALANCHEGO_VERSION=${AVALANCHEGO_VERSION:-ab83fb41528de93c1790301cdd67a07dda9299f0}
+GINKGO_VERSION=${GINKGO_VERSION:-$(getDepVersion github.com/onsi/ginkgo/v2)}
+
+# TODO: undo this hack once go.mod is referring to a tag rather than a commit
+#SUBNET_EVM_VERSION=${SUBNET_EVM_VERSION:-$(getDepVersion github.com/ava-labs/subnet-evm)}
+SUBNET_EVM_VERSION=${SUBNET_EVM_VERSION:-18633729a0cde7d695616e14b77873957a2b59c2}
 
 # Set golangci-lint version
 GOLANGCI_LINT_VERSION=${GOLANGCI_LINT_VERSION:-'v1.60'}
