9396b4b8f28370eaeb06eccd26cdde689051d8dc
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2024-08-30 00:20:32 +0000

    mv generate.sh invocation to test.sh
    addresses review comment https://github.com/ava-labs/awm-relayer/pull/457#discussion_r1736944784

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,11 @@
 # Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
 # See the file LICENSE for licensing terms.
 
-set -e errexit
-
 # Root directory
 root=$(
     cd "$(dirname "${BASH_SOURCE[0]}")"
     cd .. && pwd
 )
 
-"$root"/scripts/generate.sh
 "$root"/scripts/build_relayer.sh
 "$root"/scripts/build_signature_aggregator.sh
