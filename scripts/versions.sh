4ee587d9a921c2f6f7aae1949ece8d0c34d9d0cb
Ian Suvak <ian.suvak@avalabs.org>
2024-09-09 10:24:30 -0400

    Bump teleporter to v1.0.7

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -17,11 +17,9 @@ function getDepVersion() {
 export GO_VERSION=${GO_VERSION:-$(getDepVersion go)}
 
 # Don't export them as they're used in the context of other calls
-# TODO: undo this hack once go.mod is referring to a tag rather than a commit
 AVALANCHEGO_VERSION=${AVALANCHEGO_VERSION:-$(getDepVersion github.com/ava-labs/avalanchego)}
 GINKGO_VERSION=${GINKGO_VERSION:-$(getDepVersion github.com/onsi/ginkgo/v2)}
 
-# TODO: undo this hack once go.mod is referring to a tag rather than a commit
 SUBNET_EVM_VERSION=${SUBNET_EVM_VERSION:-$(getDepVersion github.com/ava-labs/subnet-evm)}
 
 # Set golangci-lint version
