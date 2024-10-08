0e3656e95bc4d94684e30363d1025c84a9fc2b81
Ian Suvak <ian.suvak@avalabs.org>
2024-10-08 08:08:15 -0400

    make proposer height cache query for each block

--- signature-aggregator/aggregator/proposer_height_cache.go
+++ signature-aggregator/aggregator/proposer_height_cache.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"errors"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/ava-labs/avalanchego/utils/linked"
@@ -16,31 +17,46 @@ import (
 	"go.uber.org/zap"
 )
 
-var errEmptyProposerHeightCache = errors.New("empty proposer height cache")
+var (
+	errEmptyProposerHeightCache = errors.New("empty proposer height cache")
+	errFailedToGetCurrentHeight = errors.New("failed to get current P-chain height")
+)
 
 const pChainLookback = 30 * time.Second
 
 type ProposerHeightCache struct {
-	logger           logging.Logger
-	pChainClient     validators.CanonicalValidatorClient
+	logger       logging.Logger
+	pChainClient validators.CanonicalValidatorClient
+	// protected by timeToHeightLock
 	timeToHeight     *linked.Hashmap[time.Time, uint64]
 	updateInterval   time.Duration
-	timeToHeightLock sync.Mutex
+	timeToHeightLock sync.RWMutex
+
+	// value kept separately since we might end up with an empty cache if there have been no new blocks within the pChainLookback period
+	// needs to be accessed in a thread-safe manner
+	currentMaxHeight *uint64
 }
 
 func newProposerHeightCache(
 	logger logging.Logger,
 	pChainClient validators.CanonicalValidatorClient,
 	updateInterval time.Duration,
-) *ProposerHeightCache {
+) (*ProposerHeightCache, error) {
 	pHeightCache := &ProposerHeightCache{
 		logger:         logger,
 		pChainClient:   pChainClient,
 		timeToHeight:   linked.NewHashmap[time.Time, uint64](),
 		updateInterval: updateInterval,
 	}
+	// Do an initial update to populate the cache
+	// and set initial [currentMaxHeight] value
+	// TODO: Consider doing specialized initialization that populates cache with values up to [pChainLookback] in the past
 	pHeightCache.updateData()
-	return pHeightCache
+	if atomic.LoadUint64(pHeightCache.currentMaxHeight) == 0 {
+		return nil, errFailedToGetCurrentHeight
+
+	}
+	return pHeightCache, nil
 }
 
 func (p *ProposerHeightCache) Start(ctx context.Context) {
@@ -61,55 +77,76 @@ func (p *ProposerHeightCache) Start(ctx context.Context) {
 }
 
 func (p *ProposerHeightCache) updateData() {
-	p.timeToHeightLock.Lock()
-	defer p.timeToHeightLock.Unlock()
-
 	height, err := p.pChainClient.GetCurrentHeight(context.Background())
 	if err != nil {
 		p.logger.Warn("Failed to get P-Chain height", zap.Error(err))
 		return
 	}
-	// If the newest height is already in the cache, no need to update
-	_, newestHeight, ok := p.timeToHeight.Newest()
-	if ok && newestHeight == height {
+	// If currentMaxHeight is already in the cache, no need to update
+	// or evict old entries.
+	currentMaxHeight := atomic.LoadUint64(p.currentMaxHeight)
+	if currentMaxHeight == height {
 		return
 	}
 
-	// Clean up any heights outside of the lookback period
+	for i := currentMaxHeight; i < height; i++ {
+		err := p.writeTimeForHeight(i)
+		if err != nil {
+			// Log the warning and continue
+			p.logger.Warn("Failed to write time for height", zap.Uint64("height", i), zap.Error(err))
+		}
+	}
+
+	p.evictExpired()
+}
+
+func (p *ProposerHeightCache) evictExpired() {
+	p.timeToHeightLock.Lock()
+	defer p.timeToHeightLock.Unlock()
+
 	it := p.timeToHeight.NewIterator()
 	for it.Next() {
 		if time.Since(it.Key()) > pChainLookback {
 			p.timeToHeight.Delete(it.Key())
+		} else {
+			// The cache will be strictly increasing in time, so we can break as soon as we find a time that is not too old
+			return
 		}
 	}
+}
 
+func (p *ProposerHeightCache) writeTimeForHeight(height uint64) error {
+	p.timeToHeightLock.Lock()
+	defer p.timeToHeightLock.Unlock()
 	blockBytes, err := p.pChainClient.GetBlockByHeight(context.Background(), height)
 	if err != nil {
 		p.logger.Warn("Failed to get P-Chain block by height", zap.Error(err))
-		return
+		return err
 	}
 
 	parsedBlock, err := block.Parse(block.Codec, blockBytes)
 	if err != nil {
 		p.logger.Warn("failed to parse platformvm block", zap.Error(err))
-		return
+		return err
 	}
 	// Convert to banff block to get access to Timestamp method
 	banffBlock, ok := parsedBlock.(*block.BanffStandardBlock)
 	if !ok {
 		p.logger.Warn("failed to convert to banff block")
-		return
+		return err
 	}
 	banffBlockTime := banffBlock.Timestamp()
-	p.timeToHeight.Put(banffBlockTime, height)
 
+	p.timeToHeight.Put(banffBlockTime, height)
+	atomic.StoreUint64(p.currentMaxHeight, height)
+	return nil
 }
 
 // GetOptimalHeight returns a best guess for a proposerVM height of the P-chain
 // using the most recent block time that is at least pChainLookback in the past.
 func (p *ProposerHeightCache) GetOptimalHeight() (uint64, error) {
-	p.timeToHeightLock.Lock()
-	defer p.timeToHeightLock.Unlock()
+	p.timeToHeightLock.RLock()
+	defer p.timeToHeightLock.RUnlock()
 
 	if p.timeToHeight.Len() == 0 {
 		return 0, errEmptyProposerHeightCache
@@ -129,5 +166,5 @@ func (p *ProposerHeightCache) GetOptimalHeight() (uint64, error) {
 
 	// If this is reached the cache only contains entries older than pChainLookback
 	// so we return the parent of the most recent height
-	return height - 1, nil
+	return atomic.LoadUint64(p.currentMaxHeight) - 1, nil
 }
