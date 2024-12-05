9e134e7d5a11c6be51303eacdd12c6daa93d8491
Ian Suvak <ian.suvak@avalabs.org>
2024-12-05 10:46:14 -0500

    remove TODO

--- relayer/listener.go
+++ relayer/listener.go
@@ -204,8 +204,6 @@ func (lstnr *Listener) processLogs(ctx context.Context) error {
 				zap.String("sourceBlockchainID", lstnr.sourceBlockchain.GetBlockchainID().String()),
 				zap.Error(err),
 			)
-			// TODO try to resubscribe in perpetuity once we have a mechanism for refreshing state
-			// variables such as Quorum values and processing missed blocks.
 			err = lstnr.reconnectToSubscriber()
 			if err != nil {
 				lstnr.healthStatus.Store(false)
