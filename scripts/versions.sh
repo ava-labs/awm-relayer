cd7fbf8d8152aa53c2e44dc03c8e5f215512c411
Ian Suvak <ian.suvak@avalabs.org>
2024-12-10 09:11:18 -0500

    update versions.sh

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -19,10 +19,12 @@ export GO_VERSION=${GO_VERSION:-$(getDepVersion go)}
 # Don't export them as they're used in the context of other calls
 AVALANCHEGO_VERSION=${AVALANCHEGO_VERSION:-$(getDepVersion github.com/ava-labs/avalanchego)}
 # Temporarily hardcode the Avalanchego version until outbound networking relaxation is available
-AVALANCHEGO_VERSION=v1.12.0-fuji
+AVALANCHEGO_VERSION=v1.12.0
 GINKGO_VERSION=${GINKGO_VERSION:-$(getDepVersion github.com/onsi/ginkgo/v2)}
 
 SUBNET_EVM_VERSION=${SUBNET_EVM_VERSION:-$(getDepVersion github.com/ava-labs/subnet-evm)}
+# Temporarily harcode the Subnet EVM version until there is a tagged release
+SUBNET_EVM_VERSION=6c98da796f359335f2dcfd1151af191584be8d74
 
 # Set golangci-lint version
 GOLANGCI_LINT_VERSION=${GOLANGCI_LINT_VERSION:-'v1.60'}
